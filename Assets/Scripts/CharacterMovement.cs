ddf8992381e0ea13f5f65a3d1ad5ace5429ebba8
difficu1t <46785792+difficu1t@users.noreply.github.com>
2022-05-01 18:45:46 +0300

    Сделал анимацию ходьбы

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -9,10 +9,15 @@ public class CharacterMovement : MonoBehaviour
     private Rigidbody2D _rb;
     private Animator _animator;
     private SpriteRenderer _sprite;
-
+    private AnimState _animState
+    {
+        get { return (AnimState)_animator.GetInteger("State"); }
+        set { _animator.SetInteger("State", (int)value); }
+    }
 
     public void Move(Vector2 direction)
     {
+        ActionOnDirection(direction.x, direction.y);
         _rb.velocity = new Vector2(_speed * direction.x, _speed * direction.y);
     }
 
@@ -22,45 +27,56 @@ public class CharacterMovement : MonoBehaviour
         _animator = GetComponent<Animator>();
         _sprite = GetComponentInChildren<SpriteRenderer>();
     }
+   
 
     private void ActionOnDirection(float x, float y)
     {
-        if (x < 0)
+        if (x < 0.0f)
             _sprite.flipX = true;
-        else if (x > 0)
+        else if (x > 0.0f)
             _sprite.flipX = false;
         switch (Mathf.Abs(x), y)
         {
             case (0.0f, 0.0f):
                 {
-                    // Idle
+                    _animState = AnimState.Idle;
                     break;
                 }
             case (0.0f, -1.0f):
                 {
-                    // Down
+                    _animState = AnimState.WalkDown;
                     break;
                 }
             case (0.0f, 1.0f):
                 {
-                    // Up
+                    _animState = AnimState.WalkUp;
                     break;
                 }
             case (1.0f, 0.0f):
                 {
-                    // Right
+                    _animState = AnimState.WalkSide;
                     break;
                 }
             case (1.0f, -1.0f):
                 {
-                    // Right down
+                    _animState = AnimState.WalkSideDown;
                     break;
                 }
             case (1.0f, 1.0f):
                 {
-                    // Right up
+                    _animState = AnimState.WalkSIdeUp;
                     break;
                 }
         }
+    }
+
+    public enum AnimState
+    {
+        Idle = 0, 
+        WalkSide = 1,
+        WalkDown = 2,
+        WalkUp = 3,
+        WalkSideDown = 4,
+        WalkSIdeUp = 5
     }
 }

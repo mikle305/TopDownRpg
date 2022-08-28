85b3c07b69fa902168b4839ee3e669e857a97b16
Mikhail <mikle3305@gmail.com>
2022-08-28 18:01:39 +0300

    Interactive color highlighter

---
+++ Assets/Scripts/Interactive/Highlighter.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ColorHighlighter: MonoBehaviour
+{
+    [SerializeField] private float _duration;
+    [SerializeField] private Color _targetColor;
+    private Color _defaultColor;
+    
+    private SpriteRenderer _sprite;
+    private Tweener _tweener;
+
+    public void Do()
+    {
+        _tweener?.Kill();
+        _tweener = _sprite.DOColor(_targetColor, _duration);
+    }
+
+    public void Undo()
+    {
+        _tweener?.Kill();
+        _tweener = _sprite.DOColor(_defaultColor, _duration);
+    }
+
+    private void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        _defaultColor = _sprite.color;
+    }
+}
